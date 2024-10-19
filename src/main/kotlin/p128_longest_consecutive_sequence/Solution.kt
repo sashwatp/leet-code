71ddb3bb2a099044fd3b94192fe0950ae8745a1d
Sashwat Prakash <sashwat.p@gmail.com>
2024-10-19 15:53:33 +0530

    Code for p128

---
+++ src/main/kotlin/p128_longest_consecutive_sequence/Solution.kt
@@ -0,0 +1,34 @@
+package p128_longest_consecutive_sequence
+
+import kotlin.math.max
+
+fun main() {
+    val intArray = intArrayOf(100, 4, 200, 1, 3, 2)
+    println(Solution().longestConsecutive(intArray))
+}
+
+class Solution {
+    fun longestConsecutive(nums: IntArray): Int {
+        var maxLength = 0
+
+        val numSet = mutableSetOf<Int>()
+        for (n in nums) {
+            numSet.add(n)
+        }
+
+        for (n in numSet) {
+            if (!numSet.contains((n - 1))) {
+                var start = n
+                var length = 1
+                while (numSet.contains(++start)) {
+                    length++
+                }
+
+                if (length > maxLength) {
+                    maxLength = length
+                }
+            }
+        }
+        return maxLength
+    }
+}
